283a2ad90319fa82ba1a0f281b3ef88f95b43a1e
Yunteng Gu <ak1945@qq.com>
2018-07-12 21:31:56 -0400

    saving merge.R

--- merge.R
+++ merge.R
@@ -14,8 +14,9 @@ df <- read.csv('census.csv')
 
 ####################################
 ##### write output file ############
-# add your R code to write output file
+write.csv(df, file = 'merged_parcel.csv')
 ####################################
+
 
 
 
