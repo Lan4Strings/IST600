ea7a3bc69e8b0baea5d61314f5467c6c0db6642c
Yunteng Gu <ak1945@qq.com>
2018-07-12 21:30:40 -0400

    saving cleancensus.R

--- cleancensus.R
+++ cleancensus.R
@@ -14,7 +14,8 @@ df <- read.csv('censusdata.csv')
 
 ####################################
 ##### write output file ############
-# add your R code to write output file
+write.csv(df, file = 'census.csv')
 ####################################
+
 
 
