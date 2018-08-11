a9abb8badfbcf8c8019be68411cc9bfe3e88bd67
Yunteng Gu <ak1945@qq.com>
2018-08-11 17:45:24 -0400

    Add files via upload

--- merge.R
+++ merge.R
@@ -81,7 +81,7 @@ df2<-datamerged
 
 
 
-####merge on block level
+##merge census data
 #adding latitude and longitude to the block level addresses
 #Obtaining the corresponding latitude and longitude for the block level addresses of Group A & B merged dataset
 #or just load the dataset if already done that
@@ -138,10 +138,65 @@ for(i in 1:nrow(Data_C))
   }
 }
 
-df1<-Data_C
+#Data_C provides the method to merge census data and parcel data, but in the analysis, we didn't use census data(in the ny2010.ur1.rar), thus nothing futher merging was done
+Data_C
 
 
 
+
+################ merge on block level
+
+blocklevel<-datamerged[,c("block.address","VacantBuil","SURA","Units","YearBuilt","AssessedVa","Aggravated.assault","Arson","Burglary","Larceny",
+                          "Murder","Robbery","Vehicle.theft","Total")]
+
+countY<-function(x){
+  return(sum(x=='Y'))
+}
+
+countrate<-function(x){
+  return(countY(x)/length(x))
+}
+
+meanYear<-function(x){
+  x[x==0]<-NA
+  med<-median(x,na.rm = 1)
+  x[is.na(x)]<-med
+  return(mean(x))
+}
+
+vpr<-tapply(blocklevel$VacantBuil, INDEX=blocklevel$block.address, FUN=countrate)
+opr<-1-vpr
+countall<-tapply(blocklevel$VacantBuil, INDEX=blocklevel$block.address, FUN=length)
+countSURA<-tapply(blocklevel$SURA, INDEX=blocklevel$block.address, FUN=countY)
+meanUnits<-tapply(blocklevel$Units, INDEX=blocklevel$block.address, FUN=mean)
+meanYearBuilt<-tapply(blocklevel$YearBuilt, INDEX=blocklevel$block.address, FUN=meanYear)
+meanAssessedVa<-tapply(blocklevel$AssessedVa, INDEX=blocklevel$block.address, FUN=mean)
+sumAggravated.assault<-tapply(blocklevel$Aggravated.assault, INDEX=blocklevel$block.address, FUN=sum)
+sumArson<-tapply(blocklevel$Arson, INDEX=blocklevel$block.address, FUN=sum)
+sumBurglary<-tapply(blocklevel$Burglary, INDEX=blocklevel$block.address, FUN=sum)
+sumLarceny<-tapply(blocklevel$Larceny, INDEX=blocklevel$block.address, FUN=sum)
+sumMurder<-tapply(blocklevel$Murder, INDEX=blocklevel$block.address, FUN=sum)
+sumRobbery<-tapply(blocklevel$Robbery, INDEX=blocklevel$block.address, FUN=sum)
+sumVehicle.theft<-tapply(blocklevel$Vehicle.theft, INDEX=blocklevel$block.address, FUN=sum)
+sumTotal<-tapply(blocklevel$Total, INDEX=blocklevel$block.address, FUN=sum)
+
+blocklevel<-data.frame(block.address=rownames(countSURA),
+                       Vacant_Pr=vpr,
+                       Occupied_Pr=opr,
+                       NumofBuilding=countall,
+                       SURA=countSURA,
+                       Units=meanUnits,
+                       YearBuilt=meanYearBuilt,
+                       AssessedVa=meanAssessedVa,
+                       Aggravated.assault=sumAggravated.assault,
+                       Arson=sumArson,
+                       Burglary=sumBurglary,
+                       Larceny=sumLarceny,
+                       Murder=sumMurder,
+                       Robbery=sumRobbery,
+                       Vehicle.theft=sumVehicle.theft,
+                       Total=sumTotal)
+df1<-blocklevel
 ## end your R code and logic 
 
 
